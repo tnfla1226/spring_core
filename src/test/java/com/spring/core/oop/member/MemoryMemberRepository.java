10e977517507260100c2a94c399922c7097fd8ba
tnfla1226 <tnfla1226@naver.com>
2021-09-24 11:48:36 +0900

    [21.09.24] # 초기 설정 커밋

---
+++ src/test/java/com/spring/core/oop/member/MemoryMemberRepository.java
@@ -0,0 +1,13 @@
+package com.spring.core.oop.member;
+
+public class MemoryMemberRepository implements MemberRepository {
+    @Override
+    public boolean save(Member member) {
+        return false;
+    }
+
+    @Override
+    public Member findById(Long id) {
+        return null;
+    }
+}
